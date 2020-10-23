1caa9a7735ce34dfcd28ac47853a0af198b5350e
René Ortega <renysh_12007@hotmail.com>
2020-10-22 20:03:08 -0500

    finalizado

--- lib/src/radio_app/view/pages/station_detail_page.dart
+++ lib/src/radio_app/view/pages/station_detail_page.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_examples/src/radio_app/bloc/station_provider.dart';
 import 'package:flutter_examples/src/radio_app/view/widgets/avatar_widget.dart';
+import 'package:flutter_examples/src/radio_app/view/widgets/controls_section.dart';
+import 'package:provider/provider.dart';
 
 class StationDetailPage extends StatelessWidget {
   @override
@@ -55,10 +58,118 @@ class StationDetailPage extends StatelessWidget {
                   ],
                 ),
               ),
-              Text(
-                'Playing now',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              )
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: size.width,
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Playing now',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 35.0,
+                          ),
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Icon(
+                              Icons.favorite,
+                              color: Color(0XFFFF296D),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          'assets/images/radio_app/frame_equalizer.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        Provider.of<StationProvider>(context)
+                            .selectedStation
+                            .frecuence,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        Provider.of<StationProvider>(context)
+                            .selectedStation
+                            .name,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Spacer(),
+                      Container(
+                        width: double.infinity,
+                        padding:
+                            const EdgeInsets.only(right: 30.0, bottom: 30.0),
+                        child: Row(
+                          children: [
+                            Spacer(),
+                            Container(
+                              width: 100,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    'Top 20',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Best Music',
+                                    textAlign: TextAlign.justify,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Colors.white.withOpacity(0.4),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                child: ControlsSection(),
+              ),
             ],
           ),
         ],
